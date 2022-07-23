c76b664a498dbd79a1eee05cf11914b2a8cd0f5d
davron <ergashevdavron948@gmail.com>
2022-07-23 16:34:06 +0300

    davron

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var a = +prompt ('3 ta son kiriting') 
+var b = +prompt ('2-chi sonni kiriting')
+var c = +prompt ('3- chi sonni kiriting')
+if(a > b < c) {
+    alert(b)
+} 
+else if(a < b < c){
+    alert(b)
+}
